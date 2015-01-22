2d6bbf308b31f5ff0a2091ce0d5e35288e60af80
Joe Brandt <jbrandt@nrao.edu>
2015-01-22 16:13:31 -0500

    Setting up autoconf build system.

---
+++ autogen.sh
@@ -0,0 +1,49 @@
+#!/bin/sh
+#===============================================================================
+#
+# Copyright (C) 2015 Associated Universities, Inc. Washington DC, USA.
+#
+# This program is free software; you can redistribute it
+# and/or modify it under the terms of the GNU General Public
+# License as published by the Free Software Foundation; either
+# version 2 of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will
+# be useful, but WITHOUT ANY WARRANTY; without even the implied
+# warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General
+# Public License along with this program; if not, write to the Free
+# Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+#
+# Correspondence concerning GBT software should be addressed as follows:
+#   GBT Operations
+#   National Radio Astronomy Observatory
+#   P. O. Box 2
+#   Green Bank, WV 24944-0002 USA
+#
+
+command -v libtool >/dev/null 2>&1
+if  [ $? -ne 0 ]; then
+    echo "autogen.sh: error: could not find libtool.  libtool is required to run autogen.sh." 1>&2
+    exit 1
+fi
+
+command -v autoreconf >/dev/null 2>&1
+if [ $? -ne 0 ]; then
+    echo "autogen.sh: error: could not find autoreconf.  autoconf and automake are required to run autogen.sh." 1>&2
+    exit 1
+fi
+
+mkdir -p ./config
+if [ $? -ne 0 ]; then
+    echo "autogen.sh: error: could not create directory: ./config." 1>&2
+    exit 1
+fi
+
+autoreconf --install --force --verbose -I config
+if [ $? -ne 0 ]; then
+    echo "autogen.sh: error: autoreconf exited with status $?" 1>&2
+    exit 1
+fi
