d67ba79755ba72b2862342c9dccfa737e527c215
Ramon Creager <rcreager@nrao.edu>
2017-02-06 10:11:10 -0500

    Moved 2 conversion utilities into header to allow use elsewhere.

--- src/matrix_util.cc
+++ src/matrix_util.cc
@@ -38,47 +38,6 @@ using namespace std;
 namespace mxutils
 {
 
-/**
- * This is a predicate function, intended to return true if a particular
- * char 'c' should be stripped out of the string. Here we are stripping
- * out non-numeric characters. The characters needed by the conversion
- * routines below are [0-9], [A-Fa-f], '.+-', (and 'e' for exponent,
- * already covered. So far no need for 'x' in 0x hex designation).
- *
- * @param c: the character
- *
- * @return true if the character should be stripped out.
- *
- */
-
-
-    bool is_non_numeric_p(char c)
-    {
-        static char cs[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
-                            'A', 'B', 'C', 'D', 'E', 'F',
-                            'a', 'b', 'c', 'd', 'e', 'f',
-                            '.', '+', '-'};
-        static set<char> syms(cs, cs + sizeof cs);
-        return syms.find(c) == syms.end();
-    }
-
-/**
- * This helper will strip out whatever characters the predicate
- * 'strip_p' returns 'true' for (see above).
- *
- * @param s: the string to be stripped
- *
- * @return a new string, which is 's' minus the non-numeric characters.
- *
- */
-
-    string strip_non_numeric(const string &s)
-    {
-        string stripped = s;
-        remove_if(stripped.begin(), stripped.end(), is_non_numeric_p);
-        return stripped;
-    }
-
 /********************************************************************
  * ToHex(const string &s, bool upper_case)
  *
